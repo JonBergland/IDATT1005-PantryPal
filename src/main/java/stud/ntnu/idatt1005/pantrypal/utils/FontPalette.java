417110eedb0a98eddd9743f3d6f7348e35fac324
Jon Bergland <jobergl@stud.ntnu.no>
2024-03-05 14:24:04 +0100

    Default font class

---
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/utils/FontPalette.java
@@ -0,0 +1,11 @@
+package stud.ntnu.idatt1005.pantrypal.utils;
+
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public final class FontPalette {
+  public static final Font HEADER = Font.font("", FontWeight.EXTRA_BOLD, 34);
+  public static final Font BUTTON = Font.font("", FontWeight.BOLD , 20);
+  public static final Font TEXT = Font.font("", FontWeight.MEDIUM, 16);
+  public static final Font SMALL = Font.font("", FontWeight.NORMAL, 14);
+}
