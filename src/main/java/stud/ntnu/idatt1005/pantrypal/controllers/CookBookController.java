8562d7fd3fb3fe58fe661ed1b0e60aaef4a03994
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-03-12 19:10:45 +0100

    Feat: Create temporary test data

--- src/main/java/stud/ntnu/idatt1005/pantrypal/controllers/CookBookController.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/controllers/CookBookController.java
@@ -1,16 +1,35 @@
 package stud.ntnu.idatt1005.pantrypal.controllers;
 
 import stud.ntnu.idatt1005.pantrypal.enums.Route;
+import stud.ntnu.idatt1005.pantrypal.models.Recipe;
 import stud.ntnu.idatt1005.pantrypal.utils.ViewManager;
-import stud.ntnu.idatt1005.pantrypal.views.CookBookView;
+import stud.ntnu.idatt1005.pantrypal.views.CookbookView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CookBookController extends Controller {
 
-  private final CookBookView view;
+  private final CookbookView view;
+  private HashMap<String, Recipe> recipes;
 
   public CookBookController(ViewManager viewManager) {
     super(viewManager);
-    this.view = new CookBookView(this);
+    this.recipes = new HashMap<>();
+
+    for (int i = 1; i <= 18; i++) {
+      Recipe recipe = new Recipe("Recipe " + i, null, null);
+      addRecipe(recipe);
+    }
+    this.view = new CookbookView(this);
     this.viewManager.addView(Route.COOKBOOK, this.view);
+  }
+
+  public Map<String, Recipe> getRecipes() {
+    return this.recipes;
+  }
+
+  public void addRecipe(Recipe recipe) {
+    this.recipes.put(recipe.getName(), recipe);
   }
 }
