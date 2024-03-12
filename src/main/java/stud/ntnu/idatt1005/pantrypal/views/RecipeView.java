b8c4ee55768117abbc3e916623029b50f0554854
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-03-12 09:20:48 +0100

    Refactor: Update RecipeView to extend View super class

--- src/main/java/stud/ntnu/idatt1005/pantrypal/views/RecipeView.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/views/RecipeView.java
@@ -1,25 +1,20 @@
 package stud.ntnu.idatt1005.pantrypal.views;
 
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.BorderPane;
-import stud.ntnu.idatt1005.pantrypal.views.components.NavBar;
+/**
+ * This class represents the RecipeView in the application.
+ * It extends the View class and sets the scene for the stage.
+ * The RecipeView is created with a specific viewType.
+ */
+public class RecipeView extends View {
 
-public class RecipeView extends Scene {
-
-  public RecipeView(BorderPane root, double width, double height) {
-    super(root, width, height);
-
-    NavBar navBar = new NavBar();
-    root.setTop(navBar.getNavBar());
-
-
-    VBox buttonContainer = new VBox(10);
-
-    root.setLeft(buttonContainer);
-
+  /**
+   * Constructor for the RecipeView class.
+   * Calls the superclass constructor with a specific viewType.
+   *
+   * @param viewType Type of the view to be created.
+   */
+  public RecipeView(ViewType viewType) {
+    super(viewType);
   }
 
 }
