becf443f75ecf8ae0b53f9748a45408d5a16ec31
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-03-12 09:20:30 +0100

    Refactor: Update HomeView to extend View super class

--- src/main/java/stud/ntnu/idatt1005/pantrypal/views/HomeView.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/views/HomeView.java
@@ -1,49 +1,17 @@
 package stud.ntnu.idatt1005.pantrypal.views;
 
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import stud.ntnu.idatt1005.pantrypal.views.components.NavBar;
+/**
+ * This class represents the HomeView in the application.
+ * It extends the View class and sets the scene for the stage.
+ * The HomeView is created with the HOME viewType.
+ */
+public class HomeView extends View {
 
-public class HomeView extends Scene {
-  private final Button addGroceryButton;
-  private final Button addRecipeButton;
-
-  public HomeView(double width, double height) {
-    super(new BorderPane(), width, height);
-
-    BorderPane root = (BorderPane) getRoot();
-
-    NavBar navBar = new NavBar();
-    root.setTop(navBar.getNavBar());
-
-    addGroceryButton = new Button("Add Grocery");
-    addRecipeButton = new Button("Add Recipe");
-
-    VBox buttonContainer = new VBox(10);
-    buttonContainer.getChildren().addAll(
-        addGroceryButton,
-        addRecipeButton
-    );
-    root.setLeft(buttonContainer);
-  }
-
-  public Button getAddGroceryButton() {
-    return addGroceryButton;
-  }
-
-  public Button getAddRecipeButton() {
-    return addRecipeButton;
-  }
-
-  public static HomeView create(Stage stage) {
-    Rectangle2D primaryScreenBounds = javafx.stage.Screen.getPrimary().getVisualBounds();
-    HomeView homeView = new HomeView(primaryScreenBounds.getWidth(), 650);
-    stage.setScene(homeView);
-    stage.show();
-    return homeView;
+  /**
+   * Constructor for the HomeView class.
+   * Calls the superclass constructor with ViewType as HOME.
+   */
+  public HomeView() {
+    super(ViewType.HOME);
   }
 }
