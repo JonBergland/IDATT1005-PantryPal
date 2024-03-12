522abd4140045f0e0771d2cd0ba0acd84c2e7d03
Jon Bergland <jobergl@stud.ntnu.no>
2024-03-12 16:40:04 +0100

    Refactor: Make class extend Register

--- src/main/java/stud/ntnu/idatt1005/pantrypal/registers/GroceryRegister.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/registers/GroceryRegister.java
@@ -1,24 +1,19 @@
 package stud.ntnu.idatt1005.pantrypal.registers;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import stud.ntnu.idatt1005.pantrypal.models.Grocery;
+import stud.ntnu.idatt1005.pantrypal.models.Model;
 
 /**
  * Represents a register of groceries.
  * The register is a map of grocery names and grocery objects.
  */
-public class GroceryRegister {
-  Map<String, Grocery> register;
+public class GroceryRegister extends Register{
 
   /**
    * Constructor for the GroceryRegister class
    */
   public GroceryRegister() {
-    this.register = new HashMap<>();
+    super();
   }
 
   /**
@@ -26,7 +21,7 @@ public class GroceryRegister {
    * @param register the register to be used
    */
   public GroceryRegister(GroceryRegister register) {
-    this.register = new HashMap<>(register.getRegister());
+    super(register);
   }
 
   /**
@@ -36,67 +31,25 @@ public class GroceryRegister {
    * @return the grocery with the specified name
    */
   public Grocery getGrocery(String name) throws IllegalArgumentException {
-    if (!register.containsKey(name)) {
+    if (!getRegisterMap().containsKey(name)) {
       throw new IllegalArgumentException("Grocery does not exist in register");
     }
-    return register.get(name);
+    return (Grocery) getItem(name);
   }
-
-  /**
-   * Get a deep copy of the register map
-   * @return the register map
-   */
-  public Map<String, Grocery> getRegister() {
-    return new HashMap<>(register);
-  }
-
 
   /**
    * Add a grocery to the register
    *
-   * @param grocery the grocery to be added
+   * @param model the model to be added
    * @throws IllegalArgumentException if the grocery already exists in the register
    */
-  public void addGrocery(Grocery grocery) throws IllegalArgumentException {
-    if (register.containsKey(grocery.getName())) {
+  @Override
+  public void addItem(Model model) throws IllegalArgumentException {
+    Grocery grocery = (Grocery) model;
+    if (getRegisterMap().containsKey(grocery.getKey())) {
       throw new IllegalArgumentException("Grocery already exists in register");
     }
 
-    register.put(grocery.getName(), new Grocery(grocery));
-  }
-
-  /**
-   * Remove a grocery from the register
-   *
-   * @param name the name of the grocery to be removed
-   * @throws IllegalArgumentException if the grocery does not exist in the register
-   */
-  public void removeGrocery(String name) {
-    if (!register.containsKey(name)) {
-      throw new IllegalArgumentException("Grocery does not exist in register");
-    }
-    register.remove(name);
-  }
-
-  /**
-   * Sorts the groceries in the register in ascending order based on their names.
-   *
-   * @return a list of groceries sorted in ascending order
-   */
-  public List<Grocery> sortGroceriesAscending() {
-    List<Grocery> sortedGroceries = new ArrayList<>(register.values());
-    sortedGroceries.sort(Comparator.comparing(Grocery::getName));
-    return sortedGroceries;
-  }
-
-  /**
-   * Sorts the groceries in the register in descending order based on their names.
-   *
-   * @return a list of groceries sorted in descending order
-   */
-  public List<Grocery> sortGroceriesDescending() {
-    List<Grocery> sortedGroceries = new ArrayList<>(register.values());
-    sortedGroceries.sort(Comparator.comparing(Grocery::getName).reversed());
-    return sortedGroceries;
+    getRegisterMap().put(grocery.getKey(), new Grocery(grocery));
   }
 }
