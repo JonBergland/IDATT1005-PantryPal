cf12a185e2f410623cd77cc4aee9f586e1f6e110
Jon Bergland <jobergl@stud.ntnu.no>
2024-03-12 12:39:02 +0100

    Refactor: Rename methods from Observer to Controller

--- src/main/java/stud/ntnu/idatt1005/pantrypal/views/Observable.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/views/Observable.java
@@ -11,12 +11,12 @@ public interface Observable {
    * Adds an observer to the observable
    * @param controller the observer to be added
    */
-  void addObserver(Controller controller);
+  void addController(Controller controller);
 
   /**
    * Removes an observer from the observable
    * @param controller the observer to be removed
    */
-  void removeObserver(Controller controller);
+  void removeController(Controller controller);
 
 }
