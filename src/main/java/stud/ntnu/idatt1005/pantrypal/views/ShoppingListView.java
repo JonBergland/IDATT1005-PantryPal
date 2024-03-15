e7124c56f96fd07dea98f5b673f6cde847c78c30
Jon Bergland <jobergl@stud.ntnu.no>
2024-03-15 14:48:34 +0100

    Feat: Add render-method to update the view

--- src/main/java/stud/ntnu/idatt1005/pantrypal/views/ShoppingListView.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/views/ShoppingListView.java
@@ -1,7 +1,11 @@
 package stud.ntnu.idatt1005.pantrypal.views;
 
+import javafx.scene.layout.VBox;
 import stud.ntnu.idatt1005.pantrypal.controllers.ShoppingListController;
 import stud.ntnu.idatt1005.pantrypal.enums.Route;
+import stud.ntnu.idatt1005.pantrypal.models.Grocery;
+import stud.ntnu.idatt1005.pantrypal.models.Model;
+import stud.ntnu.idatt1005.pantrypal.views.components.ShoppingListElement;
 
 /**
  * A class that extends View and creates a view for the shopping list.
@@ -25,5 +29,19 @@ public class ShoppingListView extends View {
   public ShoppingListView(ShoppingListController controller) {
     super(controller, Route.SHOPPING_LIST, "/styles/shopping-list.css");
     this.controller = controller;
+    render();
+  }
+
+  public void render() {
+    VBox vBox = new VBox();
+
+    // render the scene
+    for (Model m : controller.getRegister().getRegisterMap().values()) {
+      Grocery g = (Grocery) m;
+      ShoppingListElement element = new ShoppingListElement(g);
+      element.addObserver(controller);
+      vBox.getChildren().add(element.getPane());
+    }
+    getBorderPane().setCenter(vBox);
   }
 }
