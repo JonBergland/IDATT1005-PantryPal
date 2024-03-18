3357a7c15669e59ccc1c99030a17c1b75807cd0d
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-03-18 12:10:24 +0100

    Feat: update models and registers to new standard

--- src/main/java/stud/ntnu/idatt1005/pantrypal/registers/GroceryRegister.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/registers/GroceryRegister.java
@@ -1,55 +1,56 @@
 package stud.ntnu.idatt1005.pantrypal.registers;
 
 import stud.ntnu.idatt1005.pantrypal.models.Grocery;
-import stud.ntnu.idatt1005.pantrypal.models.Model;
+
+import java.util.LinkedHashMap;
 
 /**
  * Represents a register of groceries.
  * The register is a map of grocery names and grocery objects.
  */
-public class GroceryRegister extends Register{
+public class GroceryRegister extends Register<Grocery> {
 
-  /**
-   * Constructor for the GroceryRegister class
-   */
-  public GroceryRegister() {
-    super();
-  }
-
-  /**
-   * Deep-copy constructor for the GroceryRegister class
-   * @param register the register to be used
-   */
-  public GroceryRegister(GroceryRegister register) {
-    super(register);
-  }
-
-  /**
-   * Get a grocery from the register
-   *
-   * @param name the name of the grocery to be retrieved
-   * @return the grocery with the specified name
-   */
-  public Grocery getGrocery(String name) throws IllegalArgumentException {
-    if (!getRegisterMap().containsKey(name)) {
-      throw new IllegalArgumentException("Grocery does not exist in register");
-    }
-    return (Grocery) getItem(name);
-  }
-
-  /**
-   * Add a grocery to the register
-   *
-   * @param model the model to be added
-   * @throws IllegalArgumentException if the grocery already exists in the register
-   */
-  @Override
-  public void addItem(Model model) throws IllegalArgumentException {
-    Grocery grocery = (Grocery) model;
-    if (getRegisterMap().containsKey(grocery.getKey())) {
-      throw new IllegalArgumentException("Grocery already exists in register");
+    /**
+     * Constructor for the GroceryRegister class
+     */
+    public GroceryRegister() {
+        super();
     }
 
-    getRegisterMap().put(grocery.getKey(), new Grocery(grocery));
-  }
+    /**
+     * Deep-copy constructor for the GroceryRegister class
+     *
+     * @param register the register to be used
+     */
+    public GroceryRegister(GroceryRegister register) {
+        super(register);
+    }
+
+    public LinkedHashMap<String, Grocery> getGroceryRegister() {
+        return super.getRegister();
+    }
+
+    /**
+     * Get a grocery from the register
+     *
+     * @param name the name of the grocery to be retrieved
+     * @return the grocery with the specified name
+     */
+    public Grocery getGrocery(String name) throws IllegalArgumentException {
+        return super.getModel(name);
+    }
+
+    /**
+     * Add a grocery to the register
+     *
+     * @param grocery the grocery to be added
+     * @throws IllegalArgumentException if the grocery already exists in the register
+     */
+    public void addGrocery(Grocery grocery) {
+        super.addModel(grocery);
+    }
+
+    public void removeGrocery(Grocery grocery) {
+        super.removeModel(grocery);
+    }
 }
--- src/main/java/stud/ntnu/idatt1005/pantrypal/registers/RecipeRegister.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/registers/RecipeRegister.java
@@ -1,6 +1,5 @@
 package stud.ntnu.idatt1005.pantrypal.registers;
 
-import stud.ntnu.idatt1005.pantrypal.models.Model;
 import stud.ntnu.idatt1005.pantrypal.models.Recipe;
 
 
@@ -9,78 +8,30 @@ import stud.ntnu.idatt1005.pantrypal.models.Recipe;
  * It contains a register of recipes structured in a hashmap.
  * Goal: act as a register for recipes.
  */
-public class RecipeRegister extends Register {
-  /**
-   * Constructor for the RecipeRegister class.
-   * Initializes the recipe register.
-   */
-  public RecipeRegister() {
-    super();
-  }
-
-  /**
-   * Deep-copy constructor for the RecipeRegister class.
-   * @param register the register to be used.
-   */
-  public RecipeRegister(RecipeRegister register) {
-    super(register);
-  }
-
-  /**
-   * Add a recipe to the recipe register.
-   *
-   * @param model the recipe to be added to the recipe register.
-   */
-  @Override
-  public void addItem(Model model) throws IllegalArgumentException {
-    Recipe recipe = (Recipe) model;
-    if (super.registerMap.containsKey(recipe.getKey())) {
-      throw new IllegalArgumentException("Recipe already exists in register");
+public class RecipeRegister extends Register<Recipe> {
+    /**
+     * Constructor for the RecipeRegister class.
+     * Initializes the recipe register.
+     */
+    public RecipeRegister() {
+        super();
     }
-    super.registerMap.put(recipe.getKey(), recipe);
-  }
 
-  /**
-   * Add a recipe to the recipe register.
-   *
-   * @param name the name of the recipe.
-   * @param groceries the groceries needed for the recipe.
-   * @param steps the steps needed to make the recipe.
-   */
-  public void addRecipe(String name, GroceryRegister groceries, StepRegister steps)
-      throws IllegalArgumentException {
-    if (super.registerMap.containsKey(name)) {
-      throw new IllegalArgumentException("Recipe already exists in register");
+    /**
+     * Deep-copy constructor for the RecipeRegister class.
+     *
+     * @param register the register to be used.
+     */
+    public RecipeRegister(RecipeRegister register) {
+        super(register);
     }
-    super.registerMap.put(name, new Recipe(name, groceries, steps));
-  }
 
-  /**
-   * Update a recipe in the recipe register.
-   *
-   * @param recipe the recipe to be updated in the recipe register.
-   */
-  public void updateRecipe(Recipe recipe)
-      throws IllegalArgumentException {
-    if (!super.registerMap.containsKey(recipe.getKey())) {
-      throw new IllegalArgumentException("Recipe does not exist in register");
+    /**
+     * Add a recipe to the recipe register.
+     *
+     * @param recipe the recipe to be added to the recipe register.
+     */
+    public void addRecipe(Recipe recipe) throws IllegalArgumentException {
+        super.addModel(recipe);
     }
-    super.registerMap.replace(recipe.getKey(), recipe);
-  }
-
-  /**
-   * Update a recipe in the recipe register.
-   *
-   * @param name the name of the recipe.
-   * @param groceries the groceries needed for the recipe.
-   * @param steps the steps needed to make the recipe.
-   */
-  public void updateRecipe(String name, GroceryRegister groceries, StepRegister steps)
-      throws IllegalArgumentException{
-    if (!super.registerMap.containsKey(name)) {
-      throw new IllegalArgumentException("Recipe does not exist in register");
-    }
-    super.registerMap.replace(name, new Recipe(name, groceries, steps));
-  }
 }
-
