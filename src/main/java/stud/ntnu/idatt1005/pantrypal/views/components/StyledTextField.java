ab9e89ab7a26a2b4bc3b780c05848eb8d7af4b29
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-03-20 12:54:34 +0100

    Refactor: Remove unused features in StyledTextField component class

--- src/main/java/stud/ntnu/idatt1005/pantrypal/views/components/StyledTextField.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/views/components/StyledTextField.java
@@ -1,29 +1,12 @@
 package stud.ntnu.idatt1005.pantrypal.views.components;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
-import stud.ntnu.idatt1005.pantrypal.utils.ColorPalette;
-import stud.ntnu.idatt1005.pantrypal.utils.FontPalette;
 
 /**
  * A custom TextField with a default style and a search style.
  * Goal: Create a custom TextField with a default style and a search style.
  */
 public class StyledTextField extends TextField {
-  /**
-   * Enum for the different variants of the text field.
-   */
-  public enum Variant {
-    DEFAULT, SEARCH
-  }
 
   /**
    * Constructor for StyledTextField.
@@ -37,16 +20,4 @@ public class StyledTextField extends TextField {
     this.getStyleClass().add("text-field");
   }
 
-  /**
-   * Constructor for StyledTextField.
-   * Sets the default style for the text field.
-   *
-   * @param promptText The text to be displayed on the text field.
-   * @param variant The variant of the text field.
-   */
-  public StyledTextField(String promptText, Variant variant) {
-    super();
-    this.setPromptText(promptText);
-    this.getStyleClass().add("text-field");
-  }
 }
