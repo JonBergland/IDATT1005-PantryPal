c79388b3339d06b5801ac627cab3ef2ea01dd58d
Jon Bergland <jobergl@stud.ntnu.no>
2024-03-27 16:58:28 +0100

    Feat: Add functionality for adding Groceries to pantry

--- src/main/java/stud/ntnu/idatt1005/pantrypal/controllers/ShoppingListController.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/controllers/ShoppingListController.java
@@ -1,13 +1,15 @@
 package stud.ntnu.idatt1005.pantrypal.controllers;
 
-import java.util.Map;
-
 import stud.ntnu.idatt1005.pantrypal.enums.ButtonEnum;
 import stud.ntnu.idatt1005.pantrypal.enums.Route;
 import stud.ntnu.idatt1005.pantrypal.models.Grocery;
 import stud.ntnu.idatt1005.pantrypal.registers.GroceryRegister;
 import stud.ntnu.idatt1005.pantrypal.utils.ViewManager;
 import stud.ntnu.idatt1005.pantrypal.views.ShoppingListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Controller class for the ShoppingListView.
@@ -26,23 +28,34 @@ public class ShoppingListController extends Controller implements Observer {
   private final GroceryRegister register;
 
   /**
+   * The controller related to the pantry.
+   * Used to update the pantry view when groceries are added to the shopping list.
+   */
+  private final PantryController pantryController;
+
+  /**
    * Constructs a new ShoppingListController with a given view manager.
    *
    * @param viewManager The view manager for the application.
    */
-  public ShoppingListController(ViewManager viewManager) {
+  public ShoppingListController(ViewManager viewManager, PantryController pantryController) {
     super(viewManager);
     this.register = new GroceryRegister();
+    this.pantryController = pantryController;
     // Add some groceries to the register
-    this.register.addGrocery(new Grocery("Milk", 1, "Dairy", false));
-    this.register.addGrocery(new Grocery("Bread", 1, "Bread", false));
-    this.register.addGrocery(new Grocery("Butter", 1, "Dairy", false));
-    this.register.addGrocery(new Grocery("Cheese", 1, "Dairy", false));
-    this.register.addGrocery(new Grocery("Egg", 1, "Dairy", false));
-    this.register.addGrocery(new Grocery("Apple", 1, "Fruit", false));
-    this.register.addGrocery(new Grocery("Banana", 1, "Fruit", false));
+    String fridge = "Fridge";
+    String cupboard = "Cupboard";
+    this.register.addGrocery(new Grocery("Milk", 1, fridge, false));
+    this.register.addGrocery(new Grocery("Bread", 1, cupboard, false));
+    this.register.addGrocery(new Grocery("Butter", 1, fridge, false));
+    this.register.addGrocery(new Grocery("Cheese", 1, fridge, false));
+    this.register.addGrocery(new Grocery("Egg", 1, fridge, false));
+    this.register.addGrocery(new Grocery("Apple", 1, cupboard, false));
+    this.register.addGrocery(new Grocery("Banana", 1, cupboard, false));
 
     this.view = new ShoppingListView(this);
+    this.view.addObserver(this);
+    rerender();
     this.viewManager.addView(Route.SHOPPING_LIST, view);
   }
 
@@ -67,19 +80,48 @@ public class ShoppingListController extends Controller implements Observer {
     switch (buttonEnum) {
       case ADD:
         register.addGrocery(grocery);
-        view.render();
+        rerender();
         break;
       case REMOVE:
         register.removeGrocery(grocery);
-        view.render();
+        rerender();
         break;
       default:
-        System.out.println("Button not supported by class");
-        break;
+        throw new IllegalArgumentException("Button not supported by class");
+    }
+  }
+
+  /**
+   * Updates the observer based on the button pressed.
+   * If the button pressed is ADD_TO_PANTRY, the groceries that are checked are added to the pantry and removed from the shopping list.
+   * The view is re-rendered after the groceries are added to the pantry.
+   *
+   * @param buttonEnum the button that was pressed
+   */
+  @Override
+  public void update(ButtonEnum buttonEnum){
+    if (Objects.requireNonNull(buttonEnum) == ButtonEnum.ADD_TO_PANTRY) {
+      addGroceriesToPantry();
+      rerender();
+    } else {
+      throw new IllegalArgumentException("Button not supported by class");
+    }
+  }
+
+  public void addGroceriesToPantry(){
+    List<Grocery> groceriesToRemove = new ArrayList<>();
+    for(Grocery grocery : register.getRegister().values()){
+      if (grocery.getChecked()) {
+        pantryController.addGrocery(grocery.getShelf(), grocery.getName(), grocery.getQuantity());
+        groceriesToRemove.add(grocery);
+      }
+    }
+    for(Grocery grocery : groceriesToRemove){
+      register.removeGrocery(grocery);
     }
   }
 
   public void rerender(){
-    view.render();
+    view.render(this.register);
   }
 }
