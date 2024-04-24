967092a3477edbcd876e79462b4262660f9d44dd
Tam Le <tamle2107@hotmail.com>
2024-04-24 17:04:27 +0200

    Refactor: Removed unused code

--- src/main/java/stud/ntnu/idatt1005/pantrypal/utils/FontPalette.java
+++ src/main/java/stud/ntnu/idatt1005/pantrypal/utils/FontPalette.java
@@ -7,8 +7,6 @@ public final class FontPalette {
   public static final Font HEADER = Font.font("Times new roman", FontWeight.MEDIUM, 100);
   public static final Font BUTTON = Font.font("", FontWeight.BOLD , 20);
   public static final Font TEXT = Font.font("", FontWeight.MEDIUM, 16);
-  public static final Font SMALL = Font.font("", FontWeight.NORMAL, 14);
-  public static final Font INPUT_PROMPT_SEARCH = Font.font("inter", FontWeight.BOLD, 70);
   public static final Font SHOPPING_LIST = Font.font("Times new roman", FontWeight.BOLD, 30);
 
 }
